661911da11daa8d0fbf3194d55772c91f9fbf3e5
FelipeMG-16 <felipemaquedag@gmail.com>
2021-07-16 13:04:22 -0500

    Impresión de 20 numeros utilizando el Ciclo FOR

---
+++ CiclosJava/src/CiclosJava.java
@@ -0,0 +1,27 @@
+/*Ejemplo de Ciclos en Java. Impresión de 20 numeros con FOR*/
+
+
+public class CiclosJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		/*
+		System.out.println(1);
+		System.out.println(2);
+		System.out.println(3);
+		System.out.println(4);
+		System.out.println(5);
+	
+		 * Ciclo FOR
+		 * for (variable de inicializacion; variable inicializacion <10; 
+		 * variable ++)
+		 * 
+		 */
+		
+		for (int i=0; i < 20; i++) {
+			System.out.println (i+1);
+		}
+	}
+}
+
